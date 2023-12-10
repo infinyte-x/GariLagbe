1ec09314c35f8ce08e57044b074d16bec29a4710
Faisal Iqbal Sajib <b-18176@mangrove.edu.bd>
2023-12-10 22:16:45 +0600

    Package Site

--- js/script.js
+++ js/script.js
@@ -69,3 +69,69 @@ top_button.addEventListener('click',function(){
   })
 })
 // -------- Back to top button end -------//
+
+
+
+// ================ Car List & Detail Page Start=================//
+let products = null;
+        // get datas from file json
+        fetch('products.json')
+            .then(response => response.json())
+            .then(data => {
+                products = data;
+                addDataToHTML();
+        })
+
+        function addDataToHTML(){
+    // remove datas default from HTML
+        let listProductHTML = document.querySelector('.listProduct');
+
+        // add new datas
+        if(products != null) // if has data
+        {
+            products.forEach(product => {
+                let newProduct = document.createElement('a');
+                newProduct.href = '/detail.html?id=' + product.id;
+                newProduct.classList.add('item');
+                newProduct.innerHTML = 
+                `<img src="${product.image}" alt="">
+                <h2>${product.name}</h2>
+                <div class="price">$${product.price}</div>`;
+                listProductHTML.appendChild(newProduct);
+
+            });
+        }
+    }
+// ================ Car List & Detail Page Start=================//
+
+// ================ Car List & Detail Page Start=================//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ================ Car List & Detail Page Start=================//
