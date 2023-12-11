f753b30393745bc33cf4b472d38cd74587222950
Faisal Iqbal Sajib <b-18176@mangrove.edu.bd>
2023-12-12 05:32:12 +0600

    Bug
    Bug

--- js/script.js
+++ js/script.js
@@ -86,23 +86,52 @@ let products = null;
     // remove datas default from HTML
         let listProductHTML = document.querySelector('.listProduct');
 
-        // add new datas
-        if(products != null) // if has data
-        {
-            products.forEach(product => {
-                let newProduct = document.createElement('a');
-                newProduct.href = '/detail.html?id=' + product.id;
-                newProduct.classList.add('item');
-                newProduct.innerHTML = 
-                `<img src="${product.image}" alt="">
-                <h2>${product.name}</h2>
-                <div class="price">$${product.price}</div>`;
-                listProductHTML.appendChild(newProduct);
-
-            });
-        }
-    }
+}
 // ================ Car List & Detail Page Start=================//
+
+
+
+let product = null;
+        // get datas from file json
+        fetch('products.json')
+            .then(response => response.json())
+            .then(data => {
+                products = data;
+                showDetail();
+        })
+
+        function showDetail(){
+    // remove datas default from HTML
+        let detail = document.querySelector('.detail');
+        let listProduct = document.querySelector('.listProduct');
+        let productId =  new URLSearchParams(window.location.search).get('id');
+        let thisProduct = products.filter(value => value.id == productId)[0];
+        //if there is no product with id = productId => return to home page
+        if(!thisProduct){
+            window.location.href = "/";
+        }
+
+        detail.querySelector('.image img').src = thisProduct.image;
+        detail.querySelector('.name').innerText = thisProduct.name;
+        detail.querySelector('.price').innerText = 'BDT' + thisProduct.price;
+        detail.querySelector('.description').innerText = 'BDT' + thisProduct.description;
+
+
+        (products.filter(value => value.id != productId)).forEach(product => {
+            let newProduct = document.createElement('a');
+            newProduct.href = '/Packages.html?id=' + product.id;
+            newProduct.classList.add('item');
+            newProduct.innerHTML = 
+            `<img src="${product.image}" alt="">
+            <h2>${product.name}</h2>
+            <div class="price">$${product.price}</div>`;
+            listProduct.appendChild(newProduct);
+        });
+    }
+
+
+
+
 
 // ================ Car List & Detail Page Start=================//
 
