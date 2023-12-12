520900b9acab416b35ed05ab00aae0d0763c7488
Faisal Iqbal Sajib <b-18176@mangrove.edu.bd>
2023-12-12 06:53:55 +0600

    Ki r korar
    hege dici javascript er karone

--- js/script.js
+++ js/script.js
@@ -45,10 +45,7 @@ window.addEventListener("scroll", ()=>{
 })
 // -------- Sticky Navbar Part End --------//
 // ====== Counter Part Start ========//
-$('.count').counterUp({
-  delay: 10,
-  time: 2000
-});
+
 // -------- Counter Part End -----------//
 // ============= back to top button Start ==============//
 let top_button  =document.querySelector(".top_button")
@@ -86,52 +83,60 @@ let products = null;
     // remove datas default from HTML
         let listProductHTML = document.querySelector('.listProduct');
 
-}
-// ================ Car List & Detail Page Start=================//
+        // add new datas
+        if(products != null) // if has data
+        {
+            products.forEach(product => {
+                let newProduct = document.createElement('a');
+                newProduct.href = '/Packages.html?id=' + product.id;
+                newProduct.classList.add('item');
+                newProduct.innerHTML = 
+                `<img src="${product.image}" alt="">
+                <h2>${product.name}</h2>
+                <div class="price">$${product.price}</div>`;
+                listProductHTML.appendChild(newProduct);
 
-
-
-let product = null;
-        // get datas from file json
-        fetch('products.json')
-            .then(response => response.json())
-            .then(data => {
-                products = data;
-                showDetail();
-        })
-
-        function showDetail(){
-    // remove datas default from HTML
-        let detail = document.querySelector('.detail');
-        let listProduct = document.querySelector('.listProduct');
-        let productId =  new URLSearchParams(window.location.search).get('id');
-        let thisProduct = products.filter(value => value.id == productId)[0];
-        //if there is no product with id = productId => return to home page
-        // if(!thisProduct){
-        //     window.location.href = "/";
-        // }
-
-        detail.querySelector('.image img').src = thisProduct.image;
-        detail.querySelector('.name').innerText = thisProduct.name;
-        detail.querySelector('.price').innerText = 'BDT' + thisProduct.price;
-        detail.querySelector('.description').innerText = 'BDT' + thisProduct.description;
-
-
-        (products.filter(value => value.id != productId)).forEach(product => {
-            let newProduct = document.createElement('a');
-            newProduct.href = '/Packages.html?id=' + product.id;
-            newProduct.classList.add('item');
-            newProduct.innerHTML = 
-            `<img src="${product.image}" alt="">
-            <h2>${product.name}</h2>
-            <div class="price">$${product.price}</div>`;
-            listProduct.appendChild(newProduct);
-        });
+            });
+        }
     }
+// ================ Car List & Detail Page Start=================//
+// let product = null;
+//         // get datas from file json
+//         fetch('products.json')
+//             .then(response => response.json())
+//             .then(data => {
+//                 products = data;
+//                 showDetail();
+//         })
+
+//         function showDetail(){
+//     // remove datas default from HTML
+//         let detail = document.querySelector('.detail');
+//         let listProduct = document.querySelector('.listProduct');
+//         let productId =  new URLSearchParams(window.location.search).get('id');
+//         // let thisProduct = products.filter(value => value.id == productId)[0];
+//         //if there is no product with id = productId => return to home page
+//         // if(!thisProduct){
+//         //     window.location.href = "/";
+//         // }
+
+//         detail.querySelector('.image img').src = thisProduct.image;
+//         detail.querySelector('.name').innerText = thisProduct.name;
+//         detail.querySelector('.price').innerText = 'BDT' + thisProduct.price;
+//         detail.querySelector('.description').innerText = 'BDT' + thisProduct.description;
 
 
-
-
+//         (products.filter(value => value.id != productId)).forEach(product => {
+//             let newProduct = document.createElement('a');
+//             newProduct.href = '/Packages.html?id=' + product.id;
+//             newProduct.classList.add('item');
+//             newProduct.innerHTML = 
+//             `<img src="${product.image}" alt="">
+//             <h2>${product.name}</h2>
+//             <div class="price">$${product.price}</div>`;
+//             listProduct.appendChild(newProduct);
+//         });
+//     }
 
 // ================ Car List & Detail Page Start=================//
 
