a0272d4ae702ed34f8e2cabc72096652870d9c8d
Faisal Iqbal Sajib <b-18176@mangrove.edu.bd>
2023-12-24 03:26:27 +0600

    u

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 // ============ nav active  ===============>
+// const navLinkEls = document.querySelectorAll('nav-link');
+// const windowPathname = window.location.pathname;
 
+// navLinkEls.forEach(navLinkEl => {
+//   const navLinkPathname = new URL(navLinkEl.href).pathname;
+ 
+//   if ((windowPathname === navLinkPathname) || (windowPathname === '/index.html' && navLinkPathname === '/')) {
+//     navLinkEl.classList.add('active');
+//   }
+// } 
+//   )
 
 
 
