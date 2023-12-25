f77b4c5d2b42d4cbb023048eedd46e79dd4fb804
Faisal Iqbal Sajib <b-18176@mangrove.edu.bd>
2023-12-25 16:03:31 +0600

    h

--- js/script.js
+++ js/script.js
@@ -102,34 +102,34 @@ top_button.addEventListener('click',function(){
 
 
 // ================ Car List & Detail Page Start=================//
-// let products = null;
-//         // get datas from file json
-//         fetch('products.json')
-//             .then(response => response.json())
-//             .then(data => {
-//                 products = data;
-//                 addDataToHTML();
-//         })
+let products = null;
+        // get datas from file json
+        fetch('products.json')
+            .then(response => response.json())
+            .then(data => {
+                products = data;
+                addDataToHTML();
+        })
 
-//         function addDataToHTML(){
-//     // remove datas default from HTML
-//         let listProductHTML = document.querySelector('.listProduct');
+        function addDataToHTML(){
+    // remove datas default from HTML
+        let listProductHTML = document.querySelector('.listProduct');
 
-//         // add new datas
-//         if(products != null) // if has data
-//         {
-//             products.forEach(product => {
-//                 let newProduct = document.createElement('a');
-//                 newProduct.href = '/Packages.html?id=' + product.id;
-//                 newProduct.classList.add('item');
-//                 newProduct.innerHTML = 
-//                 `<img src="${product.image}" alt="">
-//                 <h2>${product.name}</h2>
-//                 <div class="price">৳${product.price}</div>`;
-//                 listProductHTML.appendChild(newProduct);
-//             });
-//         }
-//     }
+        // add new datas
+        if(products != null) // if has data
+        {
+            products.forEach(product => {
+                let newProduct = document.createElement('a');
+                newProduct.href = '/Packages.html?id=' + product.id;
+                newProduct.classList.add('item');
+                newProduct.innerHTML = 
+                `<img src="${product.image}" alt="">
+                <h2>${product.name}</h2>
+                <div class="price">৳${product.price}</div>`;
+                listProductHTML.appendChild(newProduct);
+            });
+        }
+    }
 
 
 // ================ Car List & Detail Page Start=================//
